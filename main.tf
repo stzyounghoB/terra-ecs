9fcec35712fe2e425df4154e385a1897a75f9593
stzyounghoB <youngho.cha@wemadeplay.com>
2025-02-18 10:35:47 +0900

    24

--- main.tf
+++ main.tf
@@ -1,28 +1,8 @@
-resource "aws_ecs_cluster" "yh_cluster" {  # 이름 수정 (하이픈 제거)
+resource "aws_ecs_cluster" "yh-cluster" {
   name = "yh-cluster"
 }
 
-resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecsExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
-  role       = aws_iam_role.ecs_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_ecs_task_definition" "yh_task" {  # 하이픈 제거
+resource "aws_ecs_task_definition" "yh-task" {
   family                   = "yh-task"
   container_definitions    = jsonencode([
     {
@@ -46,9 +26,9 @@ resource "aws_ecs_task_definition" "yh_task" {  # 하이픈 제거
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
 }
 
-resource "aws_ecs_service" "yh_service" {
+resource "aws_ecs_service" yh_service" {
   name            = "yh-service"
-  cluster         = aws_ecs_cluster.yh_cluster.id  # 하이픈 제거한 이름으로 수정
+  cluster         = aws_ecs_cluster.yh_cluster.id
   task_definition = aws_ecs_task_definition.yh_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
